388ca1e049a992c08f04eb7be77273cd7b845427
Bartosz Pietrucha <bartosz.pietrucha@onet.pl>
2024-10-09 12:57:52 +0200

    Picture_addjust

--- src/UI/templates/LogInPage.py
+++ src/UI/templates/LogInPage.py
@@ -24,37 +24,52 @@ class Ui_Form(object):
         if not Form.objectName():
             Form.setObjectName(u"Form")
         Form.resize(872, 489)
+        Form.setStyleSheet(u"#frame{\n"
+"background-image:url(\"C:/Programy/python/projekt/projekt/src/UI/apartment.jpg\");\n"
+"background-position: top center;\n"
+"width: 100%;\n"
+"height: 100%;\n"
+"background-repeat: no-repeat;\n"
+"}\n"
+"")
         self.horizontalLayout = QHBoxLayout(Form)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.frame_5 = QFrame(Form)
-        self.frame_5.setObjectName(u"frame_5")
-        self.frame_5.setFrameShape(QFrame.Shape.NoFrame)
-        self.frame_5.setFrameShadow(QFrame.Shadow.Raised)
-
-        self.horizontalLayout.addWidget(self.frame_5)
-
         self.frame = QFrame(Form)
         self.frame.setObjectName(u"frame")
-        self.frame.setMaximumSize(QSize(16777215, 800))
-        self.frame.setFrameShape(QFrame.Shape.NoFrame)
+        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame.setFrameShadow(QFrame.Shadow.Raised)
-        self.verticalLayout = QVBoxLayout(self.frame)
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout_3 = QHBoxLayout(self.frame)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.frame_6 = QFrame(self.frame)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setStyleSheet(u"")
+        self.frame_6.setFrameShape(QFrame.Shape.NoFrame)
+        self.frame_6.setFrameShadow(QFrame.Shadow.Raised)
+
+        self.horizontalLayout_3.addWidget(self.frame_6)
+
         self.frame_2 = QFrame(self.frame)
         self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setMaximumSize(QSize(16777215, 80))
+        self.frame_2.setMaximumSize(QSize(300, 800))
         self.frame_2.setFrameShape(QFrame.Shape.NoFrame)
         self.frame_2.setFrameShadow(QFrame.Shadow.Raised)
-
-        self.verticalLayout.addWidget(self.frame_2)
-
-        self.frame_3 = QFrame(self.frame)
+        self.verticalLayout = QVBoxLayout(self.frame_2)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.frame_3 = QFrame(self.frame_2)
         self.frame_3.setObjectName(u"frame_3")
-        self.frame_3.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_3.setMaximumSize(QSize(16777215, 80))
+        self.frame_3.setFrameShape(QFrame.Shape.NoFrame)
         self.frame_3.setFrameShadow(QFrame.Shadow.Raised)
-        self.verticalLayout_2 = QVBoxLayout(self.frame_3)
+
+        self.verticalLayout.addWidget(self.frame_3)
+
+        self.frame_4 = QFrame(self.frame_2)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Shadow.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.frame_4)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.frame_9 = QFrame(self.frame_3)
+        self.frame_9 = QFrame(self.frame_4)
         self.frame_9.setObjectName(u"frame_9")
         self.frame_9.setMaximumSize(QSize(16777215, 40))
         self.frame_9.setFrameShape(QFrame.Shape.StyledPanel)
@@ -63,13 +78,14 @@ class Ui_Form(object):
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.label_2 = QLabel(self.frame_9)
         self.label_2.setObjectName(u"label_2")
+        self.label_2.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.horizontalLayout_2.addWidget(self.label_2)
 
 
         self.verticalLayout_2.addWidget(self.frame_9)
 
-        self.frame_7 = QFrame(self.frame_3)
+        self.frame_7 = QFrame(self.frame_4)
         self.frame_7.setObjectName(u"frame_7")
         self.frame_7.setMinimumSize(QSize(0, 100))
         self.frame_7.setMaximumSize(QSize(16777215, 100))
@@ -81,12 +97,23 @@ class Ui_Form(object):
         self.frame_10.setObjectName(u"frame_10")
         self.frame_10.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame_10.setFrameShadow(QFrame.Shadow.Raised)
-        self.le_login = QLineEdit(self.frame_10)
-        self.le_login.setObjectName(u"le_login")
-        self.le_login.setGeometry(QRect(80, 0, 113, 26))
+        self.horizontalLayout_4 = QHBoxLayout(self.frame_10)
+        self.horizontalLayout_4.setSpacing(10)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
         self.label = QLabel(self.frame_10)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(10, 10, 63, 20))
+        self.label.setMinimumSize(QSize(0, 15))
+
+        self.horizontalLayout_4.addWidget(self.label)
+
+        self.le_login = QLineEdit(self.frame_10)
+        self.le_login.setObjectName(u"le_login")
+        self.le_login.setMinimumSize(QSize(0, 20))
+        self.le_login.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.le_login.setDragEnabled(False)
+
+        self.horizontalLayout_4.addWidget(self.le_login)
+
 
         self.verticalLayout_3.addWidget(self.frame_10)
 
@@ -94,19 +121,28 @@ class Ui_Form(object):
         self.frame_11.setObjectName(u"frame_11")
         self.frame_11.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame_11.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout_5 = QHBoxLayout(self.frame_11)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
         self.label_3 = QLabel(self.frame_11)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(10, 10, 63, 20))
+        self.label_3.setMinimumSize(QSize(0, 20))
+
+        self.horizontalLayout_5.addWidget(self.label_3)
+
         self.le_haslo = QLineEdit(self.frame_11)
         self.le_haslo.setObjectName(u"le_haslo")
-        self.le_haslo.setGeometry(QRect(80, 10, 113, 26))
+        self.le_haslo.setMinimumSize(QSize(0, 20))
+        self.le_haslo.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.horizontalLayout_5.addWidget(self.le_haslo)
+
 
         self.verticalLayout_3.addWidget(self.frame_11)
 
 
         self.verticalLayout_2.addWidget(self.frame_7)
 
-        self.frame_8 = QFrame(self.frame_3)
+        self.frame_8 = QFrame(self.frame_4)
         self.frame_8.setObjectName(u"frame_8")
         self.frame_8.setMaximumSize(QSize(16777215, 50))
         self.frame_8.setFrameShape(QFrame.Shape.StyledPanel)
@@ -120,33 +156,37 @@ class Ui_Form(object):
 
         self.verticalLayout_2.addWidget(self.frame_8)
 
-        self.label_4 = QLabel(self.frame_3)
+        self.label_4 = QLabel(self.frame_4)
         self.label_4.setObjectName(u"label_4")
 
         self.verticalLayout_2.addWidget(self.label_4)
 
 
-        self.verticalLayout.addWidget(self.frame_3)
-
-        self.frame_4 = QFrame(self.frame)
-        self.frame_4.setObjectName(u"frame_4")
-        self.frame_4.setMaximumSize(QSize(16777215, 80))
-        self.frame_4.setFrameShape(QFrame.Shape.NoFrame)
-        self.frame_4.setFrameShadow(QFrame.Shadow.Raised)
-
         self.verticalLayout.addWidget(self.frame_4)
+
+        self.frame_5 = QFrame(self.frame_2)
+        self.frame_5.setObjectName(u"frame_5")
+        self.frame_5.setMaximumSize(QSize(16777215, 60))
+        self.frame_5.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.frame_5.setFrameShape(QFrame.Shape.NoFrame)
+        self.frame_5.setFrameShadow(QFrame.Shadow.Raised)
+
+        self.verticalLayout.addWidget(self.frame_5)
+
+
+        self.horizontalLayout_3.addWidget(self.frame_2)
+
+        self.frame_12 = QFrame(self.frame)
+        self.frame_12.setObjectName(u"frame_12")
+        self.frame_12.setEnabled(True)
+        self.frame_12.setFrameShape(QFrame.Shape.NoFrame)
+        self.frame_12.setFrameShadow(QFrame.Shadow.Sunken)
+        self.frame_12.setLineWidth(0)
+
+        self.horizontalLayout_3.addWidget(self.frame_12)
 
 
         self.horizontalLayout.addWidget(self.frame)
-
-        self.frame_6 = QFrame(Form)
-        self.frame_6.setObjectName(u"frame_6")
-        self.frame_6.setEnabled(True)
-        self.frame_6.setFrameShape(QFrame.Shape.NoFrame)
-        self.frame_6.setFrameShadow(QFrame.Shadow.Sunken)
-        self.frame_6.setLineWidth(0)
-
-        self.horizontalLayout.addWidget(self.frame_6)
 
 
         self.retranslateUi(Form)
